a161c87b79735b133153232b21117fc8a10d13a3
Keondra Dickey <kdickey08@gmail.com>
2017-08-17 18:37:06 -0500

    added while index and js

--- projects/choose-your-own-adventure/cyoa.js
+++ projects/choose-your-own-adventure/cyoa.js
@@ -1,9 +1,16 @@
 var response = prompt("You walk into a room with a chair and a window. Type 'sit' to sit in the chair, type 'gaze' to gaze wistfully out the window and sigh");
 
 if(response === "sit"){
-    response = prompt("Here's a new prompt, with new options");
-} else if (response === "gaze") {
-    response = prompt("Here's a new prompt, with new options");
+    response = alert ("You will fall into a sunken place and life as you know it will no longer exist.");
+  }
+  else if (response === "gaze") {
+    response = prompt("You will see you future, choose 'remember' if you choose to remember your future or 'forget' is you wish to forget your future");
+}
+      if(response === "remember"){
+        response = alert ("You are now stuck in a place between time and cannot be saved.");
+}
+      else if (response === "forget") {
+            response = alert("You have now magically started your current all over-again.");
 } else {
     alert("Please type in a valid input! Refresh the page to try again.");
 }
