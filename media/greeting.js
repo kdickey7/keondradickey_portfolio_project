c55022fd6f857eb1b8f4ab7f37b0c8d85798c3ce
Keondra Dickey <kdickey08@gmail.com>
2017-08-16 19:54:55 -0500

    added choose-your-own-adventure dir html js

--- media/greeting.js
+++ media/greeting.js
@@ -1,3 +1,9 @@
-var name = prompt("Hi there! What's your name?");
+var firstName = prompt("Hi there! What's your first name?");
+var lastName = prompt("What's your last name?");
 var output = document.querySelector('#greeting');
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+
+if(firstName && lastName){
+    output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
+} else {
+    output.innerHTML = "<p>Please tell us your first and last names!</p>";
+}
